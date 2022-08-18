8df7157cdd796b6b90bcbf1f7687beb782d94917
VerLen <zlaya85@gmail.com>
2022-08-18 18:39:33 +0400

    Initial commit

---
+++ hw01_3.py
@@ -0,0 +1,4 @@
+n = int(input("Введите число - "))
+result = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
+print("Сумма чисел n + nn + nnn - %d" % result)
+
---
+++ hw01_4.py
@@ -0,0 +1,11 @@
+a = abs(int(input("Введите целое положительное число ")))
+max = a % 10
+while a >= 1:
+    a = a // 10
+    if a % 10 > max:
+        max = a % 10
+    if a > 9:
+        continue
+    else:
+        print("Максимальное цифра в числе ", max)
+        break
